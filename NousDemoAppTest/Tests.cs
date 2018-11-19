30812aad6ae4cc0f61b7d0a7ef582d45197a1ea8
sanoojkp <sanoojkp@nousinfo.com>
2018-11-19 17:14:06 +0530

    * NousDemoApp.sln: Added UI test project with 1 test case
    * Tests.cs:
    * packages.config:
    * AppInitializer.cs:
    * MainPage.xaml:
    * NousDemoAppTest.csproj:
    * packages.config:
    * NousDemoApp.csproj:
    * packages.config:
    * packages.config:
    * NousDemoApp.iOS.csproj:
    * NousDemoApp.Android.csproj:
    * AndroidManifest.xml:
    * Resource.Designer.cs:

---
+++ NousDemoAppTest/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace NousDemoAppTest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]         public void WelcomeTextIsDisplayed()         {
+            AppResult[] results3 = app.WaitForElement(c => c.Marked("CICD Demo Project!"));
+            Assert.IsTrue(results3.Any());         }
+    }
+}
