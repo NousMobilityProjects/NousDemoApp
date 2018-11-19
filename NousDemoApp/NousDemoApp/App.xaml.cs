57baa438e69200da49e511db1c29a9fa468dca70
Sharath N Kariappa <sharath.nk@Viewpoint.com>
2018-11-19 13:31:36 +0530

    Add project files.

---
+++ NousDemoApp/NousDemoApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
+
+namespace NousDemoApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=e20867f1-2613-4c4f-b9ce-5930d75329e8;" ,typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ NousDemoApp/NousDemoApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AppCenter.Analytics;
+using Xamarin.Forms;
+using Microsoft.AppCenter.Crashes;
+
+namespace NousDemoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Analytics.TrackEvent("Nous Demo Project Launched");
+            TrackCrash();
+        }
+
+        private void TrackCrash()
+        {
+            try
+            {
+                int value = 0;
+                int newValue = 5 / value;
+            }
+            catch (Exception exception)
+            {
+                Crashes.TrackError(exception);
+            }
+        }
+    }
+}
